7cd78471b405003fa043d1c16d7793326d267c7b
mjwebb1993 <mjwebb1993@yahoo.com>
2023-05-01 22:07:39 -0500

    Added Pizza API and updated views/Pizza

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -25,6 +26,42 @@ function afterRender(state) {
   });
 }
 
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`https://sc-pizza-api.onrender.com/pizzas`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            console.log("response", response);
+            store.Pizza.pizzas = response.data;
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
 router
   .on({
     "/": () => render(),
--- store/Pizza.js
+++ store/Pizza.js
@@ -1,4 +1,5 @@
 export default {
   header: "Show me the pizza",
-  view: "Pizza"
+  view: "Pizza",
+  pizzas: []
 };
