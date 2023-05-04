909f28ff6f7f66b73d338a8021b98a8a7ddff9dc
mjwebb1993 <mjwebb1993@yahoo.com>
2023-05-04 17:56:18 -0500

    Prettier

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,6 +6,8 @@ export default state => html`
     <a href="index.html">"Call to Action" "Button"</a>
   </section>
   <h3>
-    The weather in ${state.weather.city} is ${state.weather.description}. Temperature is ${state.weather.temp}F, and it feels like ${state.weather.feelsLike}F.
+    The weather in ${state.weather.city} is ${state.weather.description}.
+    Temperature is ${state.weather.temp}F, and it feels like
+    ${state.weather.feelsLike}F.
   </h3>
 `;
