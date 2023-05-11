a705e1f18aa0a7faa6ab5e91dc0a6b406d896ab2
Matt Thomas <matsinet@gmail.com>
2023-05-11 10:48:15 -0500

    Week 8 Express server with MongoDB

--- server/app.js
+++ server/app.js
@@ -1,19 +1,89 @@
-// 'Import' the http module
-const http = require("http");
-// Initialize the http server
-const server = http.createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// 'Import' the Express module instead of http
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
+// Initialize the Express application
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+
+mongoose.connect(process.env.MONGODB);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// mongodb+srv://mattthomassavvy:F0rdtruck@cluster0.14kfgh8.mongodb.net/?retryWrites=true&w=majority
+
+// Logging Middleware
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+app.get("/weather/:city", (request, response) => {
+  // Express adds a "params" Object to requests that has an matches parameter created using the colon syntax
+  const city = request.params.city;
+  const apiKey = request.query.apikey;
+  // Generate a random number to use as the temperature
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+  const min = 70;
+  const max = 90;
+  const temp = Math.floor(Math.random() * (max - min + 1) + min);
+  // handle GET request for weather with an route parameter of "city"
+  response.status(418).json({
+    current: `The weather in ${city} is ${temp} degrees today.`,
+    apikey: apiKey
+  });
+});
+
+app.post("/add", (request, response) => {
+  console.log(request.body);
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2
+  };
+  response.json(responseBody);
+});
+
+app.use("/pizzas", pizzas);
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ server/models/Pizza.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const pizzaSchema = new mongoose.Schema({
+  customer: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  crust: {
+    type: String,
+    required: true,
+    enum: ["thin", "chicago", "deep-dish", "hella-thick"]
+  },
+  cheese: {
+    type: String,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  sauce: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  toppings: [String]
+});
+
+const Pizza = mongoose.model("Pizza", pizzaSchema);
+
+module.exports = Pizza;
